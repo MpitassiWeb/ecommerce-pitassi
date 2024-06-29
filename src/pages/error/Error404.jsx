d3e7844018326c4abf67a4663a27a09537127afc
MpitasiWeb <marco_pitassi@hotmail.com>
2024-06-29 20:58:10 -0300

    Corregimos y damos estilo a la página de Error 404

--- src/pages/error/Error404.jsx
+++ src/pages/error/Error404.jsx
@@ -1,9 +1,14 @@
-import { Container } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./error404.css"
 
 export const Error404 = () => {
   return (
-    <Container>
+    <div className="container">
+      <Link to="/"><img
+        src="https://res.cloudinary.com/dqcn00akv/image/upload/v1718232855/OptiTech_Store_dn6qwe.png"
+        alt="Logo de OptiTech Store"
+      /></Link>
       <h1>Error 404 - Not Found</h1>
-    </Container>
+    </div>
   );
 };
