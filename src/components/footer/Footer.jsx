99298c28bb8d4851972cff23e6ed98241aa36b47
MpitasiWeb <marco_pitassi@hotmail.com>
2024-06-29 21:14:12 -0300

    Arrancamos el footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,28 @@
 export const Footer = () => {
+  const fecha = new Date()
   return (
     <div>
-        <h3>Footer</h3>
+      <span>Copyright © Optitech Solutions {fecha.getFullYear()}</span>
+      <nav>
+        <a
+          href="https://www.instagram.com/opti.tech.solutions"
+          alt="Enlace a Instagram de Optitech"
+        >
+          Instagram
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=61556247433513"
+          alt="Enlace a Faceboook de Optitech"
+        >
+          Facebook
+        </a>
+        <a
+          href="https://optitechsolutionsar.blogspot.com/"
+          alt="Enlace a Blogger"
+        >
+          Blogger
+        </a>
+      </nav>
     </div>
-  )
-}
+  );
+};
