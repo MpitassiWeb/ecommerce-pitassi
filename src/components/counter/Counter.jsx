4125d77003a84e78422f432d0e364c949c90eb28
MpitasiWeb <marco_pitassi@hotmail.com>
2024-06-30 22:58:47 -0300

    Damos estilo al contador

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,11 +1,17 @@
-import { Button } from "@mui/material"
+import { Button, Typography } from "@mui/material";
 
-export const Counter = ({restar, contador, sumar}) => {
+export const Counter = ({ restar, contador, sumar }) => {
   return (
-    <div style={{padding: "10px"}}>
-       <Button variant="contained" onClick={restar}>Restar</Button>
-       <h2 style={{padding: "10px"}}>{contador}</h2>
-       <Button variant="contained" onClick={sumar}>Sumar</Button> 
+    <div style={{ padding: "10px" }}>
+      <Button variant="contained" onClick={restar}>
+        -
+      </Button>
+      <Typography variant="span" p={3}>
+        {contador}
+      </Typography>
+      <Button variant="contained" onClick={sumar}>
+        +
+      </Button>
     </div>
-  )
-}
+  );
+};
--- src/components/counter/CounterContainer.jsx
+++ src/components/counter/CounterContainer.jsx
@@ -5,12 +5,12 @@ export const CounterContainer = () => {
   const [contador, setContador] = useState(1);
 
   const sumar = () => {
-    setContador(contador + 1)
-  }
+    setContador(contador + 1);
+  };
   const restar = () => {
-    if(contador > 1){
-        setContador(contador - 1)
+    if (contador > 1) {
+      setContador(contador - 1);
     }
-  }
-  return <Counter contador={contador} sumar={sumar} restar={restar}/>;
+  };
+  return <Counter contador={contador} sumar={sumar} restar={restar} />;
 };
