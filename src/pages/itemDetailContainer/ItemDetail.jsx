025cbcaef5630519b46c6dc9a8f05b8a519a1f60
MpitasiWeb <marco_pitassi@hotmail.com>
2024-06-30 23:02:53 -0300

    Editamos el item detail

--- src/pages/itemDetailContainer/ItemDetail.jsx
+++ src/pages/itemDetailContainer/ItemDetail.jsx
@@ -1,16 +1,39 @@
+import { Box, CardMedia, Container, Grid, Typography } from "@mui/material";
 import { CounterContainer } from "../../components/counter/CounterContainer";
 import { ProductCard } from "../../components/productCard/ProductCard";
+import styled from "@emotion/styled";
 
 export const ItemDetail = ({ item }) => {
+  const Img = styled("img");
   return (
     <>
-      <ProductCard
-        title={item.title}
-        img={item.img}
-        description={item.description}
-        price={item.price}
-      />
-      <CounterContainer />
+      <Container>
+        <Grid container>
+          <Grid item md={12}>
+            <Typography variant="h1">{item.title}</Typography>
+          </Grid>
+          <Grid item md={7}>
+            <img
+              src={item.img}
+              style={{ width: "90%" }}
+              alt={`Imagen de ${item.title}`}
+            />
+          </Grid>
+          <Grid item md={5} justifyContent="center" alignItems="flex-start">
+            <Typography variant="h2">$ {item.price}</Typography>
+          </Grid>
+          <Grid item md={5}>
+            <Typography variant="p">{item.description}</Typography>
+          </Grid>
+        </Grid>
+        {/* <ProductCard
+          title={item.title}
+          img={item.img}
+          description={item.description}
+          price={item.price}
+        /> */}
+        <CounterContainer />
+      </Container>
     </>
   );
 };
