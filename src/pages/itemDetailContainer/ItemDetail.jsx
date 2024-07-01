0b0bfade50c759a90eab70b71b6403bc16925e24
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-01 16:11:30 -0300

    Corregimos y damos estilos al ItemDetail

--- src/pages/itemDetailContainer/ItemDetail.jsx
+++ src/pages/itemDetailContainer/ItemDetail.jsx
@@ -1,32 +1,34 @@
 import { Box, CardMedia, Container, Grid, Typography } from "@mui/material";
 import { CounterContainer } from "../../components/counter/CounterContainer";
 import { ProductCard } from "../../components/productCard/ProductCard";
-import styled from "@emotion/styled";
 
 export const ItemDetail = ({ item }) => {
-  const Img = styled("img");
   return (
     <>
-      <Container>
-        <Grid container>
+      <Container maxWidth='lg' sx={{padding:"30px"}}>
+        <Grid container spacing={3}>
           <Grid item md={12}>
             <Typography variant="h1">{item.title}</Typography>
           </Grid>
           <Grid item md={7}>
             <img
               src={item.img}
-              style={{ width: "90%" }}
+              style={{ width: "100%" }}
               alt={`Imagen de ${item.title}`}
             />
           </Grid>
-          <Grid item md={5} justifyContent="center" alignItems="flex-start">
-            <Typography variant="h2">$ {item.price}</Typography>
-          </Grid>
-          <Grid item md={5}>
-            <Typography variant="p">{item.description}</Typography>
+          <Grid item md={5} container direction="column" spacing={5}>
+            <Grid item>
+              <Typography variant="h2">$ {item.price}</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="p">{item.description}</Typography>
+            </Grid>
+            <Grid item>
+              <CounterContainer />
+            </Grid>
           </Grid>
         </Grid>
-        <CounterContainer />
       </Container>
     </>
   );
