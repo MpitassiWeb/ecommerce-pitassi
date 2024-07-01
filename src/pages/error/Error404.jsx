40d319b68e5b2ffa317100b5fbd82d3dd1874287
MpitasiWeb <marco_pitassi@hotmail.com>
2024-06-30 23:01:21 -0300

    Damos estilo a la página de error

--- src/pages/error/Error404.jsx
+++ src/pages/error/Error404.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
-import "./error404.css"
+import "./error404.css";
+import { Typography } from "@mui/material";
 
 export const Error404 = () => {
   return (
     <div className="container">
-      <Link to="/"><img
-        src="https://res.cloudinary.com/dqcn00akv/image/upload/v1718232855/OptiTech_Store_dn6qwe.png"
-        alt="Logo de OptiTech Store"
-      /></Link>
-      <h1>Error 404 - Not Found</h1>
+      <Link to="/">
+        <img
+          src="https://res.cloudinary.com/dqcn00akv/image/upload/v1718232855/OptiTech_Store_dn6qwe.png"
+          alt="Logo de OptiTech Store"
+        />
+      </Link>
+      <Typography variant="h1">Error 404 - Not Found</Typography>
     </div>
   );
 };
