d9a2a5d46390273026b0d47818fbe28b8761f0af
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-03 19:39:08 -0300

    Quitamos el gutterBottom del h6

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -20,7 +20,6 @@ export const ProductCard = ({ id, title, img, price }) => {
             {title}
           </Typography>
           <Typography
-            gutterBottomgit
             variant="h6"
             sx={{ fontSize: "1.8em", paddingTop: "20px", textAlign: "end" }}
           >
