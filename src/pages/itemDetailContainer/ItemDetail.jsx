8d9443cc5f49e25b3fdd7a0acbeb478696af267b
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-02 22:11:45 -0300

    Separamos por líneas la descripción del producto y la mostramos con un map

--- src/pages/itemDetailContainer/ItemDetail.jsx
+++ src/pages/itemDetailContainer/ItemDetail.jsx
@@ -1,11 +1,17 @@
 import { Container, Grid, Typography } from "@mui/material";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { CounterContainer } from "../../components/counter/CounterContainer";
 
-
 export const ItemDetail = ({ item }) => {
+  let lines = [];
+  let id = 0;
+  if (item.description) {
+    lines = item.description.split("*");
+  }
+
   return (
     <>
-      <Container maxWidth='lg' sx={{padding:"30px"}}>
+      <Container maxWidth="lg" sx={{ padding: "30px" }}>
         <Grid container spacing={3}>
           <Grid item md={12}>
             <Typography variant="h1">{item.title}</Typography>
@@ -13,7 +19,7 @@ export const ItemDetail = ({ item }) => {
           <Grid item md={7}>
             <img
               src={item.img}
-              style={{ width: "100%" }}
+              style={{ width: "100%" , borderRadius: "20px"}}
               alt={`Imagen de ${item.title}`}
             />
           </Grid>
@@ -22,7 +28,14 @@ export const ItemDetail = ({ item }) => {
               <Typography variant="h2">$ {item.price}</Typography>
             </Grid>
             <Grid item>
-              <Typography variant="p">{item.description}</Typography>
+              {lines.map((line) => {
+                id++;
+                return (
+                  <Typography key={id} variant="body1">
+                    <ChevronRightIcon fontSize="small"/>{line}
+                  </Typography>
+                );
+              })}
             </Grid>
             <Grid item>
               <CounterContainer />
