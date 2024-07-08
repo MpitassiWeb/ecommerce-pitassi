235c7dc93eb8610ac57153ac7d955cb7f485a1e2
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-07 22:44:49 -0300

    Usamos el contexto Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Layout } from "./components/layout/Layout";
-import Navbar from "./components/navbar/Navbar";
+import { CartContextProvider } from "./context/CartContext";
 import { Cart } from "./pages/cart/Cart";
 import { Error404 } from "./pages/error/Error404";
 import { ItemDetailContainer } from "./pages/itemDetailContainer/ItemDetailContainer";
@@ -9,25 +9,31 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route
-            path="/"
-            element={
-              <ItemListContainer greeting={"¡Bienvenidos a Optitech Store!"} />
-            }
-          />
-          <Route
-            path="/category/:name"
-            element={
-              <ItemListContainer greeting={"¡Bienvenidos a Optitech Store!"} />
-            }
-          />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
-        </Route>
-        <Route path="*" element={<Error404 />} />
-      </Routes>
+      <CartContextProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route
+              path="/"
+              element={
+                <ItemListContainer
+                  greeting={"¡Bienvenidos a Optitech Store!"}
+                />
+              }
+            />
+            <Route
+              path="/category/:name"
+              element={
+                <ItemListContainer
+                  greeting={"¡Bienvenidos a Optitech Store!"}
+                />
+              }
+            />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
+          </Route>
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </CartContextProvider>
     </BrowserRouter>
   );
 }
--- src/components/carWidget/CarWidget.jsx
+++ src/components/carWidget/CarWidget.jsx
@@ -1,10 +1,14 @@
 import { Badge } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 
 export const CarWidget = () => {
+  const { cart } = useContext(CartContext);
+
   return (
-    <Badge badgeContent={0} showZero color="secondary">
+    <Badge badgeContent={cart.length} showZero color="secondary">
       <Link to="/cart">
         <ShoppingCartIcon color="#dddd32" />
       </Link>
