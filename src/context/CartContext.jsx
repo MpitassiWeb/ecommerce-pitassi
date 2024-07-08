e091ff13c03b25ea4bc60b6137e19c164b8b2e04
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-08 13:25:42 -0300

    Creamos la lógica del carrito de compras (Add, clear, deleteProduct)

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,9 +4,35 @@ export const CartContext = createContext();
 
 export const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
-  const addToCart = () => {};
-  const clearCart = () => {};
+  const exists = (id) => {
+    let inCar = cart.some((product) => product.id === id);
+    return inCar;
+  };
+  const addToCart = (product) => {
+    if (exists(product.id)) {
+      let newArray = cart.map((elemento) => {
+        if (elemento.id === product.id) {
+          return {
+            ...elemento,
+            quantity: elemento.quantity + product.quantity,
+          };
+        } else {
+          return elemento;
+        }
+      });
+      setCart(newArray);
+    } else {
+      setCart([...cart, product]);
+    }
+  };
+  const deleteProduct = (id) => {
+    let newCart = cart.filter((product) => product.id !== id);
+    setCart(newCart);
+  };
+  const clearCart = () => {
+    setCart([]);
+  };
 
-  let data = { cart, addToCart, clearCart };
+  let data = { cart, addToCart, clearCart, deleteProduct };
   return <CartContext.Provider value={data}>{children}</CartContext.Provider>;
 };
