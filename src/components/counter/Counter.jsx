aaef5685643000030e1b5bac507013b12b343577
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-13 21:02:29 -0300

    Agregamos leyenda de productos agregados en el contador

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -3,6 +3,9 @@ import { Box, Button, Container, Typography } from "@mui/material";
 export const Counter = ({ restar, contador, sumar, onAdd }) => {
   return (
     <Box width={230}>
+      { contador > 1 && (
+        <Typography variant="span">Ya tenés unidades en el carrito</Typography>
+      )}
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Button variant="contained" onClick={restar}>
           -
