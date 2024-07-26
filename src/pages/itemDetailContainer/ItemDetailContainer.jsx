cd837da920d8e229161c90e8878df0d1fc784ec9
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-25 22:18:36 -0300

    Consumimos la BD en el ItemDetail

--- src/pages/itemDetailContainer/ItemDetailContainer.jsx
+++ src/pages/itemDetailContainer/ItemDetailContainer.jsx
@@ -4,19 +4,25 @@ import "../../products";
 import { products } from "../../products";
 import { useParams } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
+import { bd } from "../../firebaseConfig";
+import { collection, doc, getDoc } from "firebase/firestore";
 
 export const ItemDetailContainer = () => {
   const { addToCart, getQuantity } = useContext(CartContext);
   const [item, setItem] = useState({});
   const { id } = useParams();
 
-  let initialQuantity = getQuantity(Number(id));
+  let initialQuantity = getQuantity(id);
 
   useEffect(() => {
-    let product = products.find((product) => product.id === Number(id));
-    if (product) {
-      setItem(product);
-    }
+    let refCollection = collection(bd, "products");
+    let refDoc = doc(refCollection, id);
+    let getProduct = getDoc(refDoc);
+
+    getProduct.then((res) => {
+      setItem({ ...res.data(), id: res.id });
+    });
+    
   }, [id]);
 
   const onAdd = (quantity) => {
@@ -24,5 +30,7 @@ export const ItemDetailContainer = () => {
     addToCart(itemAdd);
   };
 
-  return <ItemDetail item={item} onAdd={onAdd} initialQuantity={initialQuantity}/>;
+  return (
+    <ItemDetail item={item} onAdd={onAdd} initialQuantity={initialQuantity} />
+  );
 };
