703d45c07143386186c7fe9e85bf00af6f996267
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-25 23:37:56 -0300

    Instalamos Formik y Yup. Modificamos el formulario y agregamos las validaciones

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,51 +1,64 @@
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const Checkout = () => {
+  const { handleSubmit, handleChange, errors } = useFormik({
+    initialValues: { nombre: "", telefono: "", email: "" },
+    onSubmit: (data) => {},
+    validationSchema: Yup.object({
+      nombre: Yup.string("El nombre debe contener letras").required("Este campo es obligatorio"),
+      telefono: Yup.number("El teléfono debe contener números").required("Este campo es obligatorio"),
+      email: Yup.string().email("El email debe ser un email válido").required("Este campo es obligatorio"),
+    }),
+    validateOnChange: false,
+  });
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Typography variant="h3" textAlign="center">
-        Datos de facturación
+        Completar los datos
       </Typography>
-      <Box sx={{ padding: "10px", display: "flex", justifyContent: "center" }}>
-        <Box>
-          <TextField
-            name="nombre"
-            label="nombre"
-            variant="outlined"
-            sx={{ width: "50%", padding: "10px" }}
-          />
-          <TextField
-            name="apellido"
-            label="apellido"
-            variant="outlined"
-            sx={{ width: "50%", padding: "10px" }}
-          />
-          <TextField
-            name="direccion"
-            label="direccion"
-            variant="outlined"
-            sx={{ width: "50%", padding: "10px" }}
-          />
-          <TextField
-            name="provincia"
-            label="provincia"
-            variant="outlined"
-            sx={{ width: "50%", padding: "10px" }}
-          />
-          <TextField
-            name="ciudad"
-            label="ciudad"
-            variant="outlined"
-            sx={{ width: "50%", padding: "10px" }}
-          />
-          <TextField
-            name="cp"
-            label="cp"
-            variant="outlined"
-            sx={{ width: "50%", padding: "10px" }}
-          />
-        </Box>
+      <Box
+        sx={{
+          height: "250px",
+          padding: "10px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TextField
+          name="nombre"
+          label="nombre"
+          variant="outlined"
+          type="text"
+          onChange={handleChange}
+          sx={{ width: "50%", padding: "10px" }}
+          error={errors.nombre ? true : false}
+          helperText={errors.nombre}
+        />
+        <TextField
+          name="telefono"
+          label="telefono"
+          type="tel"
+          variant="outlined"
+          onChange={handleChange}
+          sx={{ width: "50%", padding: "10px" }}
+          error={errors.telefono ? true : false}
+          helperText={errors.telefono}
+        />
+        <TextField
+          name="email"
+          label="email"
+          type="email"
+          variant="outlined"
+          onChange={handleChange}
+          sx={{ width: "50%", padding: "10px" }}
+          error={errors.email ? true : false}
+          helperText={errors.email}
+        />
       </Box>
       <Box sx={{ padding: "10px", display: "flex", justifyContent: "center" }}>
         <Button type="submit" variant="contained">
