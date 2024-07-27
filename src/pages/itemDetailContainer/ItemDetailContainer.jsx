8f8cb2e7f7e36fbb49fde295bc9ed30ca3ff91a5
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-27 18:34:19 -0300

    Agregamos el Skeleton al ItemDetail

--- src/pages/itemDetailContainer/ItemDetailContainer.jsx
+++ src/pages/itemDetailContainer/ItemDetailContainer.jsx
@@ -4,10 +4,13 @@ import { useParams } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
 import { bd } from "../../firebaseConfig";
 import { collection, doc, getDoc } from "firebase/firestore";
+import { Container, Grid, Skeleton } from "@mui/material";
+import { styles } from "./styles";
 
 export const ItemDetailContainer = () => {
   const { addToCart, getQuantity } = useContext(CartContext);
   const [item, setItem] = useState({});
+  const [ready, setReady] = useState(false);
   const { id } = useParams();
 
   let initialQuantity = getQuantity(id);
@@ -16,11 +19,10 @@ export const ItemDetailContainer = () => {
     let refCollection = collection(bd, "products");
     let refDoc = doc(refCollection, id);
     let getProduct = getDoc(refDoc);
-
     getProduct.then((res) => {
       setItem({ ...res.data(), id: res.id });
+      setReady(true);
     });
-    
   }, [id]);
 
   const onAdd = (quantity) => {
@@ -28,7 +30,35 @@ export const ItemDetailContainer = () => {
     addToCart(itemAdd);
   };
 
-  return (
+  return ready ? (
     <ItemDetail item={item} onAdd={onAdd} initialQuantity={initialQuantity} />
+  ) : (
+    <>
+      <Container maxWidth="lg" sx={{ padding: "30px" }}>
+        <Grid container spacing={1}>
+          <Grid item md={12}>
+            <Skeleton variant="text" sx={styles.title} />
+          </Grid>
+          <Grid item md={7}>
+            <Skeleton variant="rounded" sx={styles.img} />
+          </Grid>
+          <Grid item md={5} container direction="column" spacing={8}>
+            <Grid item>
+              <Skeleton variant="text" sx={styles.price} />
+            </Grid>
+            <Grid item>
+              <Skeleton variant="text" sx={styles.description} />
+              <Skeleton variant="text" sx={styles.description} />
+              <Skeleton variant="text" sx={styles.description} />
+              <Skeleton variant="text" sx={styles.description} />
+              <Skeleton variant="text" sx={styles.description} />
+            </Grid>
+            <Grid item container justifyContent="center">
+              <Skeleton variant="rounded" sx={styles.button} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
---
+++ src/pages/itemDetailContainer/styles.js
@@ -0,0 +1,12 @@
+export let styles = {
+  title: {
+    bgcolor: "grey.600",
+    fontSize: "4.5rem",
+    margin: "auto",
+    width: "50%",
+  },
+  img: { bgcolor: "grey.600", width: "600px", height: "600px" },
+  price: { bgcolor: "grey.600", fontSize: "4rem", width: "200px" },
+  description: { bgcolor: "grey.600", fontSize: "1rem", width: "50%" },
+  button: { bgcolor: "grey.600", width: "150px", height: "50px" },
+};
