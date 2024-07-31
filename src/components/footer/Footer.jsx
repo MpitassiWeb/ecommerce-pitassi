03515a6e87c403497213580742a99a9ac202d801
MpitasiWeb <marco_pitassi@hotmail.com>
2024-07-31 20:51:25 -0300

    Creamos los estilos del Footer en Styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,22 +2,12 @@ import { Container, Typography } from "@mui/material";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import BookIcon from "@mui/icons-material/Book";
+import { styles } from "./styles";
 
 export const Footer = () => {
   const fecha = new Date();
   return (
-    <Container
-      component="footer"
-      sx={{
-        borderTop: "1px solid",
-        padding: "25px",
-        maxWidth: "sm",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: "20px",
-      }}
-    >
+    <Container component="footer" sx={styles.container}>
       <Typography variant="span">
         Copyright © Optitech Solutions {fecha.getFullYear()}
       </Typography>
---
+++ src/components/footer/styles.js
@@ -0,0 +1,11 @@
+export let styles = {
+  container: {
+    borderTop: "1px solid",
+    padding: "25px",
+    maxWidth: "sm",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "20px",
+  },
+};
